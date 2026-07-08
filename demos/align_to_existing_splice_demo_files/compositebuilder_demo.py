6f61f3d6086782a821481174943bb795301de5b6
J.B. Novak - Geology Projects <joseph.novak.geology@gmail.com>
2026-07-08 16:44:00 -0400

    Add files via upload

--- demos/align_to_existing_splice_demo_files/compositebuilder_demo.py
+++ demos/align_to_existing_splice_demo_files/compositebuilder_demo.py
@@ -7,7 +7,7 @@ Created on Mon May  4 14:05:55 2026
 
 from core_composite.io import load_from_multiple_files, load_external_affine, export_affine_table, export_and_save
 from core_composite.builder import CompositeBuilder
-from core_composite.gui import Phase3ReviewGUI
+from core_composite.gui import ReviewGUI
 
 %matplotlib qt
 
@@ -35,13 +35,13 @@ builder.add_hole(hole_C)
 builder.add_hole(hole_D)
 
 # Apply the published splice
-load_external_affine(builder, "published_upper_splice.csv")
+load_external_affine(builder, "Keigwin_splice.csv")
 
 # Run the automated alignment
 builder.optimize_off_splice(proxy='auto', str_range=(0.6, 1.05))
 
 # Review the results
-Phase3ReviewGUI(builder, proxy='auto')
+ReviewGUI(builder, proxy='auto')
 
 export_affine_table(builder)
 export_and_save(builder, active_holes = builder.holes, all_proxies = target_proxies)
